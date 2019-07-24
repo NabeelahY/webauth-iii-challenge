b8be63bb39bfff8ec55370d76c67ad7c344cfb73
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-24 15:16:57 +0100

    Create middleware for restricted routes

---
+++ auth/auth-middleware.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/users-models");
+
+module.exports = { restricted };
+async function restricted(req, res, next) {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
+    } else {
+      const loggedUser = await Users.findUserBy({ username });
+      req.user = loggedUser;
+      if (req.user && bcrypt.compareSync(password, req.user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "Unauthorized!" });
+      }
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "Server error" });
+  }
+}
